3cdbfedb2c3807edbf1cde384bf813c713602ecb
valdir-ti <valdir.ti@gmail.com>
2021-04-18 16:29:48 -0300

    refactor: make SignUpController conforms to protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { HttpRequest, HttpResponse } from '@src/presentation/protocols/http'
 import { MissingParamError } from '@src/presentation/errors/missing-param-error'
 import { badRequest } from '@src/presentation/helpers/http-helper'
-export class SignUpController {
+import { Controller } from '@src/presentation/protocols/controller'
+export class SignUpController implements Controller{
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for(const field of requiredFields){
