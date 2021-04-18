122177b438433d8594019267bbb5a6c8dd8ad211
valdir-ti <valdir.ti@gmail.com>
2021-04-18 18:24:28 -0300

    refactor: move all protocols imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 import { MissingParamError, InvalidParamError, ServerError } from '@src/presentation/errors'
-import { EmailValidator } from '@src/presentation/protocols/email-validator'
+import { EmailValidator } from '@src/presentation/protocols'
 
 interface SutTypes {
   sut: SignUpController,
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,6 @@
-import { HttpRequest, HttpResponse } from '@src/presentation/protocols/http'
+import { HttpRequest, HttpResponse, Controller, EmailValidator } from '@src/presentation/protocols'
 import { MissingParamError, InvalidParamError } from '@src/presentation/errors'
 import { badRequest, serverError } from '@src/presentation/helpers/http-helper'
-import { Controller } from '@src/presentation/protocols/controller'
-import { EmailValidator } from '@src/presentation/protocols/email-validator'
 export class SignUpController implements Controller{
   private readonly emailValidator: EmailValidator
 
